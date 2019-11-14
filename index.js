5e8fe6a6328f411a9308a30b3c7dad51eac34450
Patricia Montoya <pmontoya@lean-tech.io>
2019-11-14 11:12:34 -0500

    Initial config NodeJS Example Project

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/hola/:name', (req, res) => {
+  res.status(200).send({ message: `Hola ${req.params.name}`})
+})
+
+app.listen(port, () => {
+  console.log(`API REST corriendo en http://localhost:${port}`)
+})
