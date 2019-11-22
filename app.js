ad57522dbbb7051e15db12d1c9e66d37de72081b
Patricia Montoya <pmontoya@lean-tech.io>
2019-11-22 10:30:43 -0500

    Preparing to Heroku

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/api', api)
-app.get('/', (req, res) =>{
+
+app.get('/', (req, res) => {
   res.status(200).send({msg: 'Hello coder! welcome to this example API'})
 })
 
 app.use(function(req, res, next) {
-  return res.status(404).send({ msg: 'Route'+req.url+' Not found.' });
+  return res.status(404).send({ msg: `Route ${req.url} not found` });
 });
 
 module.exports = app
